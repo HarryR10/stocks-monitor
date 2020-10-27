61acfbd0a70d2b8a735d1654ae8607852d0a298e
igor_spirin <igor.r10@bk.ru>
2020-10-27 11:27:47 +0300

    feature (src.app) add search component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['../../styles.css']
+})
+export class SearchComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
